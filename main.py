aca56d54483eede441734c9bb15ffe7d3d342511
Beastman-Dev <wmeastman@gmail.com>
2025-03-23 10:15:15 -0400

    Created field and player objects, updatable and drawable containers, and player rotation and movement functions

--- main.py
+++ main.py
@@ -3,20 +3,37 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    updatable = pygame.sprite.Group() # All objects that can be updated
+    drawable = pygame.sprite.Group() # All objects that can be drawn
+    Player.containers = (drawable, updatable)
+
+    player: Player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill("black")
-        pygame.display.flip
+        for object in drawable:
+            object.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
